11f3280b1d0e68ccb51d78abaf02f30dc7666277
Artur Stopyra <astopyra@gmail.com>
2022-10-09 14:03:57 +0200

    proba polecenia commit -am

--- src/main/java/com/kodilla/FirstClass.java
+++ src/main/java/com/kodilla/FirstClass.java
@@ -3,5 +3,6 @@ package com.kodilla;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hallo world!!");
+        System.out.println("commit -am  -> dodaje i komituje");
     }
 }
