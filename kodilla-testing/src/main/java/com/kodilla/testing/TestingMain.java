7d1dda2d4f96c9be7bd41f2243d58b5eb11a9156
Artur Stopyra <astopyra@gmail.com>
2022-10-12 21:18:26 +0200

    6.3 JUnit testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,32 +4,6 @@ import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        //test calculator
-        Calculator calc = new Calculator();
-        int wynik = calc.add(9,11);
-
-        if(wynik == 9+11){
-            System.out.println("Calculator adding test ok");
-        }else {
-            System.out.println("Calculator adding test: error");
-        }
-
-        wynik = calc.subtract(9, 11);
-
-        if(wynik == 9-11){
-            System.out.println("Calculator subtracting test ok");
-        }else {
-            System.out.println("Calculator subtracting test: error");
-        }
     }
 }
