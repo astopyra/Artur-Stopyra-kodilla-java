a1739952d7bee7ff26053d2bffe32a8af57df43b
Artur Stopyra <astopyra@gmail.com>
2022-10-12 20:14:24 +0200

    Class calculator - unit tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //test calculator
+        Calculator calc = new Calculator();
+        int wynik = calc.add(9,11);
+
+        if(wynik == 9+11){
+            System.out.println("Calculator adding test ok");
+        }else {
+            System.out.println("Calculator adding test: error");
+        }
+
+        wynik = calc.subtract(9, 11);
+
+        if(wynik == 9-11){
+            System.out.println("Calculator subtracting test ok");
+        }else {
+            System.out.println("Calculator subtracting test: error");
+        }
     }
 }
