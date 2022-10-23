ee061c28bcada1759768edc227bd114dd30e85fd
Artur Stopyra <astopyra@gmail.com>
2022-10-23 22:55:02 +0200

    7.2 class ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,19 +1,14 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        String txt = "  <Text to beautify>  ";
-
-        poemBeautifier.beautify(txt, text -> text + "ABC" );
-        poemBeautifier.beautify(txt, text -> text.toUpperCase() );
-        poemBeautifier.beautify(txt, text -> text.trim() );
-        poemBeautifier.beautify(txt, text -> "!!" + text + "!!" );
-
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
